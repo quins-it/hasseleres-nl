d4344f3bb49bdbf8ce5b6467c14ce686e1b4dbc6
abpross <andre@pross.nl>
2024-12-24 16:06:22 +0100

    redirect

---
+++ images/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Redirect to a page</title>
+    </head>
+    <body onload="window.location.assign('https://www.hasseleres.nl')">
+
+    </body>
+</html>
+
+
